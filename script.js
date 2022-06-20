d488ebed3fd2beab72b21fd485a5f979f020c63a
icy <107867160+icyprology@users.noreply.github.com>
2022-06-20 22:36:13 +0800

    removed

--- script.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.showcase');
-const navigation = document.querySelector('.navigation');
-
-menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active');
-    showcase.classList.toggle('active');
-    navigation.classList.toggle('active');
-})
